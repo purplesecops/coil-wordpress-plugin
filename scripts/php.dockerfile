d809287d460a1cc9b2dc8371e462f4a84964b734
SarahCoilAccount <sarah@coil.com>
2021-08-20 13:14:49 +0200

    Adding in extra functionality

--- scripts/php.dockerfile
+++ scripts/php.dockerfile
@@ -12,6 +12,25 @@ RUN chmod +x wp-cli.phar
 
 RUN mv wp-cli.phar /usr/local/bin/wp
 
+RUN apt update
+
+RUN apt install default-mysql-client -y
+
+RUN apt install -y libzip-dev zip && docker-php-ext-install zip
+
+RUN wp core download --skip-content --allow-root
+
+COPY . /var/www/html/wp-content/plugins/coil-wordpress-plugin
+
+RUN cp wp-config-sample.php wp-config.php
+
+RUN sed -i "s/database_name_here/wordpress/" "wp-config.php"
+RUN sed -i "s/username_here/admin/" "wp-config.php"
+RUN sed -i "s/password_here/password/" "wp-config.php"
+RUN sed -i "s/localhost/db/" "wp-config.php"
+
+RUN bash curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash && nvm install 12 && nvm use 12
+
 # RUN wp core install --url=http://php --title=wordpress --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email  --allow-root
 
 # RUN wp plugin install wordpress-importer --activate  --allow-root
--- scripts/wordpress-set-up.sh
+++ scripts/wordpress-set-up.sh
@@ -2,30 +2,10 @@
 
 set -e
 
-apt update
-
-apt install default-mysql-client
-
-apt install -y libzip-dev zip && docker-php-ext-install zip
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-wp core download --skip-content --allow-root
-
-cp wp-config-sample.php wp-config.php
-
-sed -i "s/database_name_here/wordpress/" "wp-config.php"
-sed -i "s/username_here/admin/" "wp-config.php"
-sed -i "s/password_here/password/" "wp-config.php"
-sed -i "s/localhost/db/" "wp-config.php"
-
 wp core install --url=http://php --title=wordpress --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email  --allow-root
 
 wp plugin install wordpress-importer --activate  --allow-root
 
 wp import /var/www/html/wp-config/plugins/coil-wordpress-plugin/cypress/fixtures/coil-automation.xml --authors=create  --allow-root
 
-# wp plugin activate /var/www/html/wp-config/plugins/coil-wordpress-plugin/plugin.php --allow-root
+wp plugin activate /var/www/html/wp-config/plugins/coil-wordpress-plugin --allow-root
