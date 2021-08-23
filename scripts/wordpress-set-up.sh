b187ece1ed4903d840f31ecda28b4d79964e7ca2
SarahCoilAccount <sarah@coil.com>
2021-08-23 13:32:30 +0200

    Copied the coil-automation file and replaced all host references with php. Now tests are running correctly.

--- scripts/wordpress-set-up.sh
+++ scripts/wordpress-set-up.sh
@@ -6,11 +6,11 @@ set -e
 
 wp core install --url=http://php --title=wordpress --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email  --allow-root
 
-wp db import /var/www/html/wp-content/plugins/coil-wordpress-plugin/scripts/test-site.sql --allow-root
+# wp db import /var/www/html/wp-content/plugins/coil-wordpress-plugin/scripts/test-site.sql --allow-root
 
-# wp plugin install wordpress-importer --activate  --allow-root
+wp plugin install wordpress-importer --activate  --allow-root
 
-# wp import /var/www/html/wp-content/plugins/coil-wordpress-plugin/cypress/fixtures/coil-automation.xml --authors=create  --allow-root
+wp import /var/www/html/wp-content/plugins/coil-wordpress-plugin/cypress/fixtures/coil-automation-docker.xml --authors=create  --allow-root
 
 wp plugin activate coil-wordpress-plugin --allow-root
 
