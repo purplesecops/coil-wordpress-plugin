62359b34a6c3c3822a9f26cd826e1343e347fda6
SarahCoilAccount <sarah@coil.com>
2021-09-01 11:47:46 +0200

    Checking excerpt functionality

--- cypress/integration/frontend/excerpt.spec.js
+++ cypress/integration/frontend/excerpt.spec.js
@@ -3,8 +3,8 @@ describe('Excerpt behaviour', () => {
 		cy.logInToWordPress('admin', 'password');
 	})
 
-	it('Checks that the post excerpts respect the coil settings', () => {
-		setPostExcerptVisibility(true)
+	it('Checks that the excerpt respects coil settings', () => {
+		setExcerptVisibility(true)
 
 		cy.visit('/?p=105/');
 
@@ -12,38 +12,21 @@ describe('Excerpt behaviour', () => {
 			.contains('This content should be visible as an excerpt')
 			.should('be.visible');
 
-		setPostExcerptVisibility(false)
+		setExcerptVisibility(false)
 
 		cy.visit('/?p=105/');
 		cy
 			.contains('This content should be visible as an excerpt')
 			.should('not.be.visible');
 	})
-
-// 	it('Checks that the page excerpts respect the coil settings', () => {
-// 		setPageExcerptVisibility(true)
-
-// 		cy.visit('/?p=105/');
-
-// 		cy
-// 			.contains('This content should be visible as an excerpt')
-// 			.should('be.visible');
-
-// 		setPageExcerptVisibility(false)
-
-// 		cy.visit('/?p=105/');
-// 		cy
-// 			.contains('This content should be visible as an excerpt')
-// 			.should('not.be.visible');
-// 	})
-// })
+})
 
 /**
  * Sets whether excerpts are visible on posts
  *
  * @param {boolean} state
  */
-function setPostExcerptVisibility(state) {
+function setExcerptVisibility(state) {
 	cy.visit('/wp-admin/admin.php?page=coil_settings&tab=excerpt_settings');
 	switch (state) {
 		case true:
@@ -55,32 +38,6 @@ function setPostExcerptVisibility(state) {
 		case false:
 			cy
 				.get('#post_display_excerpt')
-				.uncheck();
-			break;
-	}
-
-	cy
-		.get('#submit')
-		.click();
-}
-
-/**
- * Sets whether excerpts are visible on pages
- *
- * @param {boolean} state
- */
- function setPageExcerptVisibility(state) {
-	cy.visit('/wp-admin/admin.php?page=coil_settings&tab=excerpt_settings');
-	switch (state) {
-		case true:
-			cy
-				.get('#page_display_excerpt')
-				.check();
-			break;
-
-		case false:
-			cy
-				.get('#page_display_excerpt')
 				.uncheck();
 			break;
 	}
