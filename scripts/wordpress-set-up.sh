5216b46fa57bd3199505e0b9a443a01f7bfa2f84
SarahCoilAccount <sarah@coil.com>
2021-09-03 15:26:30 +0200

    Adding an htaccess file

--- scripts/wordpress-set-up.sh
+++ scripts/wordpress-set-up.sh
@@ -6,15 +6,15 @@ set -e
 
 wp core install --url=http://127.0.0.1 --title=wordpress --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email  --allow-root
 
-wp db import /var/www/html/wp-content/plugins/coil-wordpress-plugin/scripts/db.sql --allow-root
+# wp db import /var/www/html/wp-content/plugins/coil-wordpress-plugin/scripts/db.sql --allow-root
 
-# wp plugin install wordpress-importer --activate  --allow-root
+wp plugin install wordpress-importer --activate  --allow-root
 
-# wp import /var/www/html/wp-content/plugins/coil-wordpress-plugin/cypress/fixtures/coil-automation-docker.xml --authors=create  --allow-root
+wp import /var/www/html/wp-content/plugins/coil-wordpress-plugin/cypress/fixtures/coil-automation-docker.xml --authors=create  --allow-root
 
 wp plugin activate coil-wordpress-plugin --allow-root
 
-cd wp-content/plugins/coil-wordpress-plugin
+# cd wp-content/plugins/coil-wordpress-plugin
 
 # npx cypress run --config baseUrl="http://php"
 # npx cypress run --spec 'cypress/integration/frontend/excerpt.spec.js' --config baseUrl="http://127.0.0.1"
