6fb1e84b674522550d5cfcc3845de8e46cf20c5f
SarahCoilAccount <sarah@coil.com>
2021-09-07 15:21:58 +0200

    Cleaned up single post tests

--- tests/cypress/integration/backend/page-visibility.spec.js
+++ tests/cypress/integration/backend/page-visibility.spec.js
@@ -5,81 +5,53 @@ describe( 'Tests for visibility settings in editor', () => {
 	} );
 
 	it( 'Checks that visibility settings of a post can be changed in Gutenberg', () => {
-		// Create a new post
-		cy.visit( '/wp-admin/post-new.php' );
-		cy.get( '#post-title-0' )
-			.click( { force: true } )
-			.type( 'Page Visibility Settings' );
+		cy.visit( '/post-visibility' );
 
-		// Set the post to Split
-		cy
-			.get( '.interface-complementary-area' )
-			.contains( 'Coil Web Monetization' )
-			.click( { force: true } );
-		cy
-			.get( '#inspector-select-control-1' )
-			.select( 'Enabled' );
-		cy
-			.get( '#inspector-radio-control-0-2' )
-			.check();
-		cy
-			.get( '.editor-post-publish-panel__toggle' )
-			.click();
-		cy
-			.get( '.editor-post-publish-panel__header-publish-button > .components-button' )
-			.click();
-		cy
-			.get( '.editor-post-publish-panel__header > .components-button' )
-			.click();
+		const monetizationDropDown = '#inspector-select-control-1';
+		const monetizationAndVisibilityCombinations = [
+			{
+				monetization: 'Enabled',
+				visibility: '#inspector-radio-control-0-2', // Enabled with Split content
+			},
+			{
+				monetization: 'Enabled',
+				visibility: '#inspector-radio-control-0-0', // Enabled for Everyone
+			},
+			{
+				monetization: 'Enabled',
+				visibility: '#inspector-radio-control-0-1', // Enabled for Coil Members Only
+			},
+			{
+				monetization: 'Disabled',
+			},
+		];
 
-		// Check the Split setting is still there
-		cy
-			.get( '#inspector-radio-control-0-2' )
-			.should( 'be.checked' );
+		monetizationAndVisibilityCombinations.foreach( ( selection ) => {
+			cy
+				.get( monetizationDropDown )
+				.select( selection.monetization );
+			if ( selection.monetization === 'Disabled' ) {
+				cy
+					.get( '.editor-post-publish-button' )
+					.click();
 
-		// Set the post to Enabled for Everyone
-		cy
-			.get( '#inspector-select-control-1' )
-			.select( 'Enabled' );
-		cy
-			.get( '#inspector-radio-control-0-0' )
-			.check();
-		cy
-			.get( '.editor-post-publish-button' )
-			.click();
+				// Check the correct setting is still there
+				cy
+					.get( monetizationDropDown )
+					.should( 'contain', selection.monetization );
+			} else {
+				cy
+					.get( selection.visibility )
+					.check();
+				cy
+					.get( '.editor-post-publish-button' )
+					.click();
 
-		// Check the Enabled for Everyone setting is still there
-		cy
-			.get( '#inspector-radio-control-0-0' )
-			.should( 'be.checked' );
-
-		// Set the post to Enabled and Exclusive
-		cy
-			.get( '#inspector-select-control-1' )
-			.select( 'Enabled' );
-		cy
-			.get( '#inspector-radio-control-0-1' )
-			.check();
-		cy
-			.get( '.editor-post-publish-button' )
-			.click();
-
-		// Check the Enabled and Exclusive setting is still there
-		cy
-			.get( '#inspector-radio-control-0-1' )
-			.should( 'be.checked' );
-
-		// Set the post to Disabled
-		cy
-			.get( '#inspector-select-control-1' )
-			.select( 'Disabled' );
-		cy
-			.get( '.editor-post-publish-button' )
-			.click();
-
-		// Check the Disabled setting is still there
-		cy
-			.get( '#inspector-select-control-1' )
-			.should( 'contain', 'Disabled' );
+				// Check the correct setting is still there
+				cy
+					.get( selection.visibility )
+					.should( 'be.checked' );
+			}
+		} );
 	} );
 } );
