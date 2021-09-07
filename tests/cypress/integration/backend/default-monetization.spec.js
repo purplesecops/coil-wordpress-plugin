26ee3aa5f389bb749a62fbf90f70e7ea84b034bd
SarahCoilAccount <sarah@coil.com>
2021-09-07 13:34:43 +0200

    Finished default monetization tests

--- tests/cypress/integration/backend/default-monetization.spec.js
+++ tests/cypress/integration/backend/default-monetization.spec.js
@@ -32,5 +32,45 @@ describe( 'Default monetization settings for pages and posts', () => {
 		// Check there is no monetization meta tag when monetization is diasabled.
 		cy.visit( '/default-monetization-post/' );
 		cy.get( 'head meta[name="monetization"]' ).should( 'not.exist' );
+		cy.visit( '/default-monetization-page/' );
+		cy.get( 'head meta[name="monetization"]' ).should( 'not.exist' );
+
+		// Set the monetization on pages and posts to enabled and public
+		cy.visit( '/wp-admin/admin.php?page=coil_settings&tab=monetization_settings' );
+		cy
+			.get( '#post_no_gating' )
+			.check();
+
+		cy
+			.get( '#page_no_gating' )
+			.check();
+		cy
+			.get( '#submit' )
+			.click();
+
+		// Check that the correct body class has been added.
+		cy.visit( '/default-monetization-post/' );
+		cy.get( 'body' ).should( 'have.class', 'coil-no-gating' );
+		cy.visit( '/default-monetization-page/' );
+		cy.get( 'body' ).should( 'have.class', 'coil-no-gating' );
+
+		// Set the monetization on pages and posts to enabled and exclusive
+		cy.visit( '/wp-admin/admin.php?page=coil_settings&tab=monetization_settings' );
+		cy
+			.get( '#post_gate_all' )
+			.check();
+
+		cy
+			.get( '#page_gate_all' )
+			.check();
+		cy
+			.get( '#submit' )
+			.click();
+
+		// Check that the correct body class has been added.
+		cy.visit( '/default-monetization-post/' );
+		cy.get( 'body' ).should( 'have.class', 'coil-gate-all' );
+		cy.visit( '/default-monetization-page/' );
+		cy.get( 'body' ).should( 'have.class', 'coil-gate-all' );
 	} );
 } );
