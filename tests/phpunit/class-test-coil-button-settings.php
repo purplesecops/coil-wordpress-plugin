34d0b8de978be995e708a287c2098407e9748b3f
SarahCoilAccount <sarah@coil.com>
2022-02-21 22:42:30 +0200

    Fixed PHPUnit tests and simplified the transfer testing around the Promotion Bar since it has been deprecated

--- tests/phpunit/class-test-coil-button-settings.php
+++ tests/phpunit/class-test-coil-button-settings.php
@@ -22,7 +22,7 @@ class Test_Coil_Button_Settings extends WP_UnitTestCase {
 
 		// Database defaults must first be setup
 		Coil\maybe_update_database();
-		$default_coil_button_display = Admin\get_coil_button_setting( 'coil_show_promotion_bar' );
+		$default_coil_button_display = Admin\get_coil_button_setting( 'coil_button_toggle' );
 
 		$this->assertSame( true, $default_coil_button_display );
 	}
@@ -34,17 +34,17 @@ class Test_Coil_Button_Settings extends WP_UnitTestCase {
 	 */
 	public function test_if_the_coil_button_display_setting_is_retrieved_successfully() :  void {
 
-		$coil_button_display = [ 'coil_show_promotion_bar' => false ];
+		$coil_button_display = [ 'coil_button_toggle' => false ];
 		update_option( 'coil_button_settings_group', $coil_button_display );
 
-		$coil_button_settings = Admin\get_coil_button_setting( 'coil_show_promotion_bar' );
+		$coil_button_settings = Admin\get_coil_button_setting( 'coil_button_toggle' );
 
 		$this->assertSame( false, $coil_button_settings );
 
-		$coil_button_display = [ 'coil_show_promotion_bar' => true ];
+		$coil_button_display = [ 'coil_button_toggle' => true ];
 		update_option( 'coil_button_settings_group', $coil_button_display );
 
-		$coil_button_settings = Admin\get_coil_button_setting( 'coil_show_promotion_bar' );
+		$coil_button_settings = Admin\get_coil_button_setting( 'coil_button_toggle' );
 
 		$this->assertSame( true, $coil_button_settings );
 	}
